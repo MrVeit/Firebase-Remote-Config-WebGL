84ad66d2349de4f2ebd44f9eb246cb9f8235dbde
Mr_Veit <veit.cooperation228@gmail.com>
2025-05-27 02:36:35 +1200

    Minor fix

--- FRC WebGL/Source/Runtime/Data/FirebaseInitConfig.cs
+++ FRC WebGL/Source/Runtime/Data/FirebaseInitConfig.cs
@@ -5,15 +5,15 @@ namespace FRCWebGL.Data
     [Serializable]
     public sealed class FirebaseInitConfig
     {
-        public string apiKey { get; set; }
-        public string authDomain { get; set; }
-        public string projectId { get; set; }
-        public string appId { get; set; }
+        public string apiKey;
+        public string authDomain;
+        public string projectId;
+        public string appId;
 
-        public string? storageBucket { get; set; }
-        public string? messagingSenderId { get; set; }
+        public string? storageBucket;
+        public string? messagingSenderId;
 
-        public long? minFetchDelayPerMillis { get; set; }
-        public long? fetchTimeoutMillis { get; set; }
+        public long? minFetchDelayPerMillis;
+        public long? fetchTimeoutMillis;
     }
 }
